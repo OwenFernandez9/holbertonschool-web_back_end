c167e2bfec2545777d958502f504c4ba5f05cf78
OwenFernandez9 <9357@holbertonstudents.com>
2024-11-14 16:39:31 -0300

    make classroom

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,9 +1,10 @@
 import Classroom from './0-classroom';
 
 export function initializeRooms() {
-  return [
+  const rooms = [
     new Classroom(19),
     new Classroom(20),
     new Classroom(34),
   ];
+  return rooms;
 }
