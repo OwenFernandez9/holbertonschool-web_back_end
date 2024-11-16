d90ab733d83ce5c14f9b8a393577573c9ad7abfd
OwenFernandez9 <9357@holbertonstudents.com>
2024-11-16 14:13:54 -0300

    pricing

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,46 @@
+import Currency from currency
+
+class Pricing {
+    constructor(amount, currency){
+        if (typeof amount !== 'number') {
+            throw TypeError('amount must be a number')
+        }
+
+        if (!(currency instanceof Currency)) {
+            throw Error('The currency parameter must be an instance of the Currency class')
+        }
+
+        this.amount = amount;
+        this.currency = currency
+    }
+
+    get amount() {
+        return this.amount;
+    }
+
+    set amount(value) {
+        if (typeof value !== 'number'){
+            throw new TypeError ('amount must be a number');
+        }
+        this.amount = value;
+    }
+
+    get currency() {
+        return this.currency;
+    }
+
+    set currency(value) {
+        if (!(value instanceof Currency)) {
+            throw Error('The currency parameter must be an instance of the Currency class')
+        }
+    }
+
+    displayFullPrice() {
+        return `${this._amount} ${this.currency.currency_name} (${this.currency.currency_code})`;
+    }
+
+    estatic convertPrice(amount, conversionRate){
+        return amount * conversionRate
+    }
+
+}
