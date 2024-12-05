d9e6f0b0a14acf41c6b5bca09ccb70b3e9fe49d8
root <root@H64.hbtn.uy>
2024-12-05 14:42:36 -0300

    add

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
+"""
+In this module, the function add is defined.
+"""
 def add(a: float, b: float) -> float:
+    """
+    The add function is used to sum a and b.
+    """
     return (a + b)
