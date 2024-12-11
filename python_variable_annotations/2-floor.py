21ed96f6cb34fecb857994af5dcd18352eba8589
root <root@H64.hbtn.uy>
2024-12-11 10:17:57 -0300

    floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+In this module, the function floor is defined.
+"""
+import math
+
+
+def floor(n: float) -> float:
+    """
+    The floor function is used to returns the floor of the float.
+    """
+    return math.floor(n)
