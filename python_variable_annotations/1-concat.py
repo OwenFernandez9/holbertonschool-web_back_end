0a1af947f52e8fb8b7ff112fb054897afbe9464f
root <root@H64.hbtn.uy>
2024-12-11 10:10:55 -0300

    concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+"""
+In this module, the function concat is defined.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    The add function is used to concat str1 and str2.
+    """
+    return str1 + str2
