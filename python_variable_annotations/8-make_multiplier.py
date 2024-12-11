8cf53bffeaa94741aaf82b6b18c263c365bebc25
root <root@H64.hbtn.uy>
2024-12-11 11:24:38 -0300

    make multiplier

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,20 @@
+#!/bin/env python3
+"""
+In this module, the function make_multiplier is defined.
+"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    make_multiplier create a function called multiplier
+    """
+    def multiplier_function(n: float) -> float:
+        """
+        multiplier_function this is the internal function/
+        that receives a number n of type float and returns the/
+        result of multiplying n by multiplier.
+        """
+        return n * multiplier
+    return multiplier_function
+
