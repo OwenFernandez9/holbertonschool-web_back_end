5b40d57fa09809c2d3c9899b276f0f13abed782b
root <root@H64.hbtn.uy>
2024-12-11 10:34:48 -0300

    sum list

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+In this module, the function sum_list is defined.
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """
+    The sum_list function is used to returns their sum as a float.
+    """
+    return sum(input_list)
