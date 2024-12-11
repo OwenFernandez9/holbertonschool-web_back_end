2556cb64b2a08820c2e3879d0638346423ac5875
root <root@H64.hbtn.uy>
2024-12-11 11:36:22 -0300

    element length

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -2,11 +2,12 @@
 """
 In this module, the function element_length is defined.
 """
-from typing import Iterable,List, Sequence, Tuple
+from typing import Iterable, List, Sequence, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """
-    The element_length function is used to return values with the appropriate types
+    The element_length function is used to return/
+    values with the appropriate types
     """
     return [(i, len(i)) for i in lst]
