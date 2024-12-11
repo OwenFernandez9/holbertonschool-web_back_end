342d7af5a2c3bbdc12c854168a9a05c0b54977c1
root <root@H64.hbtn.uy>
2024-12-11 10:44:27 -0300

    sum mixed list

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -2,10 +2,10 @@
 """
 In this module, the function sum_mixed_list is defined.
 """
-from typing import List
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[int, float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[(int, float]]) -> float:
     """
     The sum_mixed_list function is used to returns their sum as a float.
     """
