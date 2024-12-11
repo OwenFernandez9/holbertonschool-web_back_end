5263203b2389733abf96d001f28d2bdfd1198b3d
root <root@H64.hbtn.uy>
2024-12-11 10:24:43 -0300

    to_str

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -6,6 +6,7 @@ In this module, the function to_str is defined.
 
 def to_str(n: float) -> str:
     """
-    The to_str function is used to returns the string representation of the float.
+    The to_str function is used to returns the/
+    string representation of the float.
     """
     return str(n)
