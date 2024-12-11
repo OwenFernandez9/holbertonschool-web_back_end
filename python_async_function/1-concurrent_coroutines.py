54baa5dd1eadc9e6bb5785cd154be36ff646c601
root <root@H64.hbtn.uy>
2024-12-11 13:50:04 -0300

    Concurrent_coroutines

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+this module contains a function
+"""
+import asyncio
+from typing import List
+wait_random = __import__("0-basic_async_syntax").wait_random
+
+
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
+    """
+    This function sorts the contents of a list of floats
+
+    args: n - number, max_delay - max number the function
+
+    Return: sorted list
+    """
+    listfloat = []
+
+    for i in range(n):
+        listfloat.append(wait_random(max_delay))
+    x = await asyncio.gather(*listfloat)
+    return sorted(x)
