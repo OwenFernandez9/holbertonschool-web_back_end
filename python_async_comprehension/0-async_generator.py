18f06699ddc856c7e2c3028a40f61850c019db9d
root <root@H64.hbtn.uy>
2024-12-11 14:25:20 -0300

    async generator

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env pyhton3
+"""
+this module contains a function
+"""
+import asyncio
+import random
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """
+    this function return random number between 0 and 10.
+    """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
