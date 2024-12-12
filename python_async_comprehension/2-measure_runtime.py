d54f3612207aaba68e36b662b5942163aa6c6556
root <root@H64.hbtn.uy>
2024-12-12 12:09:02 -0300

    measure runtime

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+this module contains a function
+"""
+import asyncio
+import time
+async_comprehension = __import__("0-async_generator").async_comprehension
+
+
+async def measure_runtime() -> float:
+    """
+    this function should measure the total runtime and return it.
+    """
+    start_time = time.time()
+
+    asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
+    )
+    end_time = time.time()
+    total_time = (end_time - start_time)
+    return total_time
