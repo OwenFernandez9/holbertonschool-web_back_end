e4bc5e62c7f89ce088654ffd74880bde068a5073
root <root@H64.hbtn.uy>
2024-12-12 11:42:02 -0300

    async comprehension

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -3,10 +3,11 @@
 this module contains a function
 """
 import asyncio
+from typing import List
 async_generator = __import__("0-async_generator").async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """
     this function return the 10 random numbers.
     """
