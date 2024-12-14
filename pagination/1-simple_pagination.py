a139f47b0cea439f4f531286aab16ec6a786cad8
OwenFernandez9 <9357@holbertonstudents.com>
2024-12-14 23:23:01 +0000

    simple pagination

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+from typing import Tuple
+import csv
+import math
+from typing import List
+"""
+This module contains the function 'index_range'.
+"""
+
+
+def index_range(page, page_size) -> Tuple[int, int]:
+    """
+    The function should return a tuple of size two
+    containing a start index and an end index
+    corresponding to the range of indexes to return
+    in a list for those particular pagination parameters.
+    """
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        allows you to obtain a specific page of data from a data set, using the
+        page number (page) and page size (page_size) provided as parameters.
+        """
+        assert isinstance(page, int), "page must be an integer"
+        assert isinstance(page_size, int), "page_size must be an integer"
+        assert page > 0, "page must be greater than 0"
+        assert page_size > 0, "page_size must be greater than 0"
+
+        dataset = self.dataset()
+
+        start, end = index_range(page, page_size)
+
+        if start >= len(dataset):
+            return []
+        return dataset[start:end]
