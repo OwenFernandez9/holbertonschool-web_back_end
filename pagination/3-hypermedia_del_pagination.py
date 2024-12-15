4b7656bde4fa93e6ea9f5b45c30ee9a2b77a6bd2
OwenFernandez9 <9357@holbertonstudents.com>
2024-12-15 17:37:37 +0000

    hypermedia del pagination

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -2,7 +2,6 @@
 """
 Deletion-resilient hypermedia pagination
 """
-
 import csv
 import math
 from typing import List, Dict
@@ -40,6 +39,24 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """
+        The method should return a dictionary
+        with the following key-value pairs:
+        index: the current start index of the return
+        page. That is the index of the
+        first item in the current page. For example if
+        requesting page 3 with page_size
+        20, and no data was removed from the dataset,
+        he current index should be 60.
+
+        next_index: the next index to query with. That
+        should be the index of the first
+        item after the last item on the current page.
+
+        page_size: the current page size
+
+        data: the actual page of the dataset
+        """
         assert isinstance(index, int), "index must be an integer"
         assert index >= 0, "index must be greater than or equal to 0"
 
