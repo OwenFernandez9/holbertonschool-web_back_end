553e00defa7cbf8b6b815d18ed9acacd64d980ca
OwenFernandez9 <9357@holbertonstudents.com>
2025-01-22 15:03:14 -0300

    stdin

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -5,7 +5,7 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
+rl.question('Welcome to Holberton School, what is your name?\r', (name) => {
   console.log(`Your name is: ${name}`);
   console.log('This important software is now closing');
   rl.close();
