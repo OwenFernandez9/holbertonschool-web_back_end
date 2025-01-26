9ce32d886db08bf29ecb439692212a3e9ef46b44
OwenFernandez9 <9357@holbertonstudents.com>
2025-01-26 19:00:30 -0300

    read file

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf8');
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+
+    const totalStudents = lines.length - 1;
+    console.log(`Number of students: ${totalStudents}`);
+
+    const fields = {};
+    lines.slice(1).forEach((line) => {
+      const [firstname, , , field] = line.split(',');
+      if (field) {
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+        fields[field].push(firstname);
+      }
+    });
+
+    for (const field in fields) {
+      console.log(
+        `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`
+      );
+    }
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
