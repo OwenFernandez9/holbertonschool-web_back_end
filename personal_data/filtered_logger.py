8cc9439b17f777f80a04e657f01242ee779f9253
OwenFernandez9 <9357@holbertonstudents.com>
2025-07-22 10:03:41 -0300

    filtered logger

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,5 +3,4 @@ import re
 from typing import List
 
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
-    escaped_sep = re.escape(separator)
-    return re.sub(rf"({'|'.join(fields)})=.*?{separator}", rf"\1={redaction}{separator}", message)
+    return re.sub(f"({'|'.join(fields)})=.*?{separator}", f"\\1={redaction}{separator}", message)
