5666d51100faf17464d9982811a3ef165e67a7d2
OwenFernandez9 <9357@holbertonstudents.com>
2025-07-22 10:20:15 -0300

    filtered logger

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -2,5 +2,8 @@
 import re
 from typing import List
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
-    return re.sub(f"({'|'.join(fields)})=.*?{separator}", f"\\1={redaction}{separator}", message)
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    return re.sub(f"({'|'.join(fields)})=.*?{separator}",
+                  f"\\1={redaction}{separator}", message)
