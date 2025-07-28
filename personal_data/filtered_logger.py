0c73de09b5d6931ad74d12ec5ba3a30fb15d23a1
OwenFernandez9 <9357@holbertonstudents.com>
2025-07-27 22:58:12 -0300

    filtered logger

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -25,27 +25,24 @@ class RedactingFormatter(logging.Formatter):
     """
     Redacting Formatter class
     """
-
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
+    def __init__(self, fields: List[str]):
+        """
+        Initialize RedactingFormatter with fields to redact.
+        """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
 
-def __init__(self, fields: List[str]):
-    """
-    Initialize RedactingFormatter with fields to redact.
-    """
-    super(RedactingFormatter, self).__init__(self.FORMAT)
-    self.fields = fields
-
-
-def format(self, record: logging.LogRecord) -> str:
-    """
-    filter values in incoming log records using filter_datum
-    """
-    original = super().format(record)
-    return filter_datum(self.fields, self.REDACTION,
-                        original, self.SEPARATOR)
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        filter values in incoming log records using filter_datum
+        """
+        original = super().format(record)
+        return filter_datum(self.fields, self.REDACTION,
+                            original, self.SEPARATOR)
 
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
@@ -66,7 +63,7 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db() -> MySQLConnection:  # type: ignore
+def get_db() -> MySQLConnection:
     """
     Returns a connector to the database.
     """
