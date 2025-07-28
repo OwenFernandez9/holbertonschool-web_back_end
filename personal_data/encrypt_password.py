634e9d8f10d1e3dffe7a433946aab3c1103e4f03
OwenFernandez9 <9357@holbertonstudents.com>
2025-07-27 23:18:20 -0300

    encrypt password

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -10,3 +10,10 @@ def hash_password(password: str) -> bytes:
     Hashes a password using bcrypt with a salt and returns the hashed password.
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Checks if the provided password matches the hashed password.
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
