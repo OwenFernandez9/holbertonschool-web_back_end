d16c9840efea47606cae33289b66e283e85c2d20
OwenFernandez9 <9357@holbertonstudents.com>
2025-07-27 22:27:58 -0300

    filtered logger

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -27,30 +27,34 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields: List[str]):
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields = fields
 
-    def format(self, record: logging.LogRecord) -> str:
-        """
-        filter values in incoming log records using filter_datum
-        """
-        original = super().format(record)
-        return filter_datum(self.fields, self.REDACTION,
-                            original, self.SEPARATOR)
+def __init__(self, fields: List[str]):
+    super(RedactingFormatter, self).__init__(self.FORMAT)
+    self.fields = fields
 
-    PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
-    def get_logger() -> logging.Logger:
-        """
-        function returns a logging.Logger object.
-        """
-        logger = logging.getLogger("user_data")
-        logger.setLevel(logging.INFO)
-        logger.propagate = False
+def format(self, record: logging.LogRecord) -> str:
+    """
+    filter values in incoming log records using filter_datum
+    """
+    original = super().format(record)
+    return filter_datum(self.fields, self.REDACTION,
+                        original, self.SEPARATOR)
 
-        stream_handler = logging.StreamHandler()
-        stream_handler.setFormatter(RedactingFormatter(list(PII_FIELDS)))
-        logger.addHandler(stream_handler)
 
-        return logger
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
+def get_logger() -> logging.Logger:
+    """
+    function returns a logging.Logger object.
+    """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(list(PII_FIELDS)))
+    logger.addHandler(stream_handler)
+
+    return logger
