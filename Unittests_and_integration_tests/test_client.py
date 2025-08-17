f81de6d66165297b6295feac4cfe59e317c47a6a
OwenFernandez9 <9357@holbertonstudents.com>
2025-08-17 20:46:31 -0300

    test client

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -1,34 +1,31 @@
 #!/usr/bin/env python3
-import sys
-import types
 import unittest
 from unittest.mock import patch
 from parameterized import parameterized
-
-if 'requests' not in sys.modules:
-    sys.modules['requests'] = types.SimpleNamespace(get=lambda *a, **k: None)
 
 from client import GithubOrgClient
 
 
 class TestGithubOrgClient(unittest.TestCase):
+
     @parameterized.expand([
         ("google",),
         ("abc",),
     ])
-    @patch("client.get_json")
-    def test_org(self, org_name, mock_get_json):
+    def test_org(self, org_name):
         expected = {"org": org_name}
-        mock_get_json.return_value = expected
 
-        client = GithubOrgClient(org_name)
+        with patch("client.get_json") as mock_get_json:
+            mock_get_json.return_value = expected
 
-        result = client.org
-        self.assertEqual(result, expected)
+            client = GithubOrgClient(org_name)
 
-        mock_get_json.assert_called_once_with(
-            GithubOrgClient.ORG_URL.format(org=org_name)
-        )
+            result = client.org
+            self.assertEqual(result, expected)
+
+            mock_get_json.assert_called_once_with(
+                GithubOrgClient.ORG_URL.format(org=org_name)
+            )
 
 
 if __name__ == "__main__":
