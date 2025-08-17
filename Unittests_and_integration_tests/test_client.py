db23c8494cf08532e77424aa71a2b64065e133cc
OwenFernandez9 <9357@holbertonstudents.com>
2025-08-17 19:13:58 -0300

    test client

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -5,23 +5,24 @@ from parameterized import parameterized
 
 from client import GithubOrgClient
 
+
 class TestGithubOrgClient(unittest.TestCase):
     @parameterized.expand([
         ("google",),
         ("abc",),
     ])
     @patch("client.get_json")
-
     def test_org(self, org_name, mock_get_json):
         expected = {"org": org_name}
-        mock_get_json.return_value = expected\
-        
+        mock_get_json.return_value = expected
+
         client = GithubOrgClient(org_name)
         self.assertEqual(client.org, expected)
 
         mock_get_json.assert_called_once_with(
             f"https://api.github.com/orgs/{org_name}"
         )
-    
+
+
 if __name__ == "__main__":
     unittest.main()
