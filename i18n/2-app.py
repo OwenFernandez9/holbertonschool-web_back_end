a5875f4d8c7d4e5ea6bb963b06a7f9beeb7e2b1a
OwenFernandez9 <9357@holbertonstudents.com>
2025-08-17 22:05:06 -0300

    flask app

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -19,12 +19,14 @@ app.config.from_object(Config)
 
 babel = Babel()
 
+
 def get_locale():
     """
     Pick the best match between the client's Accept-Language header
     and the languages supported by our app.
     """
     return request.accept_languages.best_match(app.config["LANGUAGES"])
+
 
 babel.init_app(app, locale_selector=get_locale)
 
