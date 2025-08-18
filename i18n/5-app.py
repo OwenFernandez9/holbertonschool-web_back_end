848c4152a11f2422dfc2fa094c0a8bcfcabdf7c3
OwenFernandez9 <9357@holbertonstudents.com>
2025-08-17 23:23:03 -0300

    flask app

--- i18n/5-app.py
+++ i18n/5-app.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python3
-"""Flask+Babel: forzar locale por ?locale=xx y mock
-de login por ?login_as=<id>."""
 from flask import Flask, render_template, request, g
 from flask_babel import Babel
 
 
 class Config:
-    """Ajustes i18n."""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -26,7 +23,6 @@ babel = Babel()
 
 
 def get_user():
-    """Devuelve el usuario según ?login_as=<id> o None."""
     uid = request.args.get("login_as")
     try:
         return users.get(int(uid)) if uid is not None else None
@@ -36,16 +32,16 @@ def get_user():
 
 @app.before_request
 def before_request():
-    """Guarda el user en g.user si existe."""
     g.user = get_user()
 
 
 def get_locale():
-    """Usa ?locale=xx si es válido; si no, Accept-Language."""
     forced = request.args.get("locale")
     if forced in app.config["LANGUAGES"]:
         return forced
-    return request.accept_languages.best_match(app.config["LANGUAGES"])
+    return request.accept_languages.best_match(
+        app.config["LANGUAGES"]
+        ) or app.config["BABEL_DEFAULT_LOCALE"]
 
 
 babel.init_app(app, locale_selector=get_locale)
