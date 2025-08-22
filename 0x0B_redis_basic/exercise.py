447a47ca8ef21a993c65e4445fc5d1042e61ea75
OwenFernandez9 <9357@holbertonstudents.com>
2025-08-22 01:39:22 -0300

    exercise

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -6,6 +6,9 @@ import uuid
 from typing import Union
 
 
+T = TypeVar("T")
+
+
 class Cache:
     def __init__(self):
         self._redis = redis.Redis()
@@ -15,3 +18,15 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable[[bytes], T]] = None) -> Optional[Union[bytes, T]]:
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        return fn(data) if fn else data
+    
+    def get_str(self, key: str) -> Optional[str]:
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> Optional[int]:
+        return self.get(key, fn=int)
