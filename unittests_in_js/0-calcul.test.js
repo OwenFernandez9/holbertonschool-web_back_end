ce0b7cd224834888013c83f2146e27c8742d6878
OwenFernandez9 <9357@holbertonstudents.com>
2025-08-26 12:07:12 -0300

    unittests

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -2,19 +2,19 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', function () {
-  it('should return 4 when inputs are 1 and 3', function () {
+  it('...', function () {
     assert.strictEqual(calculateNumber(1, 3), 4);
   });
 
-  it('should return 5 when inputs are 1 and 3.7', function () {
+  it('...', function () {
     assert.strictEqual(calculateNumber(1, 3.7), 5);
   });
 
-  it('should return 5 when inputs are 1.2 and 3.7', function () {
+  it('...', function () {
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
   });
 
-  it('should return 6 when inputs are 1.5 and 3.7', function () {
+  it('...', function () {
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
 });
