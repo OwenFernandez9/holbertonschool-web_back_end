06d8bffb5919ca6090f7af6e4811224d2059fb08
OwenFernandez9 <9357@holbertonstudents.com>
2025-08-31 02:51:13 -0300

    unittests

---
+++ unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,29 @@
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
+
+describe('calculateNumber', function () {
+  describe('SUM', function () {
+    it('...', function () {
+      expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6)
+    });
+  });
+
+  describe('SUBTRACT', function () {
+    it('...', function () {
+      expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4)
+    });
+  });
+
+  describe('DIVIDE', function() {
+    it('...', function() {
+      expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2)
+    });
+  });
+  describe('DIVIDE', function() {
+    it('...', function() {
+      expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error')
+    });
+  });
+
+});
+
